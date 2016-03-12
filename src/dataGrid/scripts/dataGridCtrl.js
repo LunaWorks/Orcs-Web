78b2ea2ecbc4b2936db70a0d5e92561d23175d5b
Mark Kalinovits <kalee.mark@gmail.com>
2016-03-12 13:50:10 +0100

    Sample code

---
+++ src/dataGrid/scripts/dataGridCtrl.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var DataGridCtrl = function($scope) {
+    $scope.data = [
+        {key: 'John', value: 'Doe'},
+        {key: 'Ann', value: 'Frank'}
+    ];
+};
+
+module.exports = DataGridCtrl;
