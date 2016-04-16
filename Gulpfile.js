787c36154bf18353af4fc44b1174bd11fd884326
Mark Kalinovits <kalee.mark@gmail.com>
2016-04-16 09:54:46 +0200

    Update Gulpfile.js

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,18 +1,19 @@
+var gulp = require('gulp');
 var ftp = require('vinyl-ftp');
 var gutil = require('gulp-util');
 var minimist = require('minimist');
 var args = minimist(process.argv.slice(2));
 
 gulp.task('deploy', function() {
-  var remotePath = '/orcs/';
+  var remotePath = '/public_html/';
   var conn = ftp.create({
-    host: 'ftp.s5.domain-ellenorzes.hu',
+    host: 'wecodetheweb.com',
     user: args.user,
     password: args.password,
     log: gutil.log
   });
 
-  gulp.src(['index.html', './**/*'])
+  gulp.src(['index.html', './**/*.css'])
     .pipe(conn.newer(remotePath))
     .pipe(conn.dest(remotePath));
 });
