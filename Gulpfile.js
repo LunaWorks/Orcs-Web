896729296b7a71dbc45dc4d0a16b02e7c9c89dec
Mark Kalinovits <kalee.mark@gmail.com>
2016-04-16 10:46:08 +0200

    build tweak

--- Gulpfile.js
+++ Gulpfile.js
@@ -13,7 +13,7 @@ gulp.task('deploy', function() {
     log: gutil.log
   });
 
-  gulp.src(['./src/**/*'])
+  gulp.src(['./src/*', './src/**/*'])
     .pipe(conn.newer(remotePath))
     .pipe(conn.dest(remotePath));
 });
