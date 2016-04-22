79842651dc9662bba838992c51b7b239fed68030
Mark Kalinovits <kalee.mark@gmail.com>
2016-04-22 05:27:35 +0200

    fix

--- src/app/welcome/scripts/WelcomeCtrl.js
+++ src/app/welcome/scripts/WelcomeCtrl.js
@@ -5,7 +5,7 @@ var WelcomeCtrl = function($scope, $timeout) {
   $timeout(function() {
     $scope.splash = false;
   }, 10000);
-  $scope.testVar = 'Custom stucture is running from require module!';
+  $scope.testVar = 'Custom structure is running from require module!';
 };
 
 module.exports = WelcomeCtrl;
